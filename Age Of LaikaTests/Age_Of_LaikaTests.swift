0cf800d5600b54ef5a82bf5d7601195c8f2c6ae6
Neil Houselander <neilhouselander@sky.com>
2015-12-13 17:40:19 +0000

    Initial Commit

---
+++ Age Of LaikaTests/Age_Of_LaikaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Age_Of_LaikaTests.swift
+//  Age Of LaikaTests
+//
+//  Created by Neil Houselander on 13/12/2015.
+//  Copyright © 2015 Neil Houselander. All rights reserved.
+//
+
+import XCTest
+@testable import Age_Of_Laika
+
+class Age_Of_LaikaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
