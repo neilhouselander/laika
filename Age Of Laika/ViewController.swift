023df4ba37e6e5dc9b6bdbd5c012b19517c90d36
Neil Houselander <neilhouselander@sky.com>
2015-12-24 17:47:04 +0000

    Laika first commit

--- Age Of Laika/ViewController.swift
+++ Age Of Laika/ViewController.swift
@@ -9,6 +9,97 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ageInHumanTextField: UITextField!
+    @IBOutlet weak var ageInDogLabel: UILabel!
+    @IBOutlet weak var dogNameTextField: UITextField!
+    @IBOutlet weak var convertToDogButtonOutlet: UIButton!
+    @IBOutlet weak var covertAnotherButtonOutlet: UIButton!
+    
+    //convert age button
+    @IBAction func convertToDogButtonPressed(sender: UIButton)
+    {
+        //error handling goes here e.g. no name or age
+        
+        if (dogNameTextField.text == "")
+        {
+        //set up the error label
+        ageInDogLabel.text = "Need a name please!"
+            
+        //show the label
+        ageInDogLabel.hidden = false
+            
+        //hide convert button & show convert another
+            convertToDogButtonOutlet.hidden = true
+            covertAnotherButtonOutlet.hidden = false
+        
+        }
+        else if (ageInHumanTextField.text == "")
+        {
+            //set up the error label
+            ageInDogLabel.text = "Need an age please!"
+            
+            //show the label
+            ageInDogLabel.hidden = false
+            
+            //hide convert button & show convert another
+            convertToDogButtonOutlet.hidden = true
+            covertAnotherButtonOutlet.hidden = false
+        }
+        else{
+            
+            //grab the number & then the name
+            //NEED ERROR HANDLING HERE IF LETTERS ENTERED
+            let humanYears = Double(ageInHumanTextField.text!)
+            let dogName = dogNameTextField.text
+            var answer: Double
+            
+            //convert and output. Logic-> dogs age 10.5 years per year 1st 2 years, then 4 years for each year
+            
+            if humanYears <= 2
+            {
+                answer = humanYears! * 10.5
+            }
+            else{
+                answer = ((humanYears! - 2) * 4 ) + 21
+            }
+            
+            ageInDogLabel.text = "\(dogName! ) is \(answer ) years old !"
+            
+            //tidy up
+            ageInHumanTextField.resignFirstResponder()
+            
+            //hide the text fields
+            ageInHumanTextField.hidden = true
+            dogNameTextField.hidden = true
+            
+            //show the label
+            ageInDogLabel.hidden = false
+            
+            //hide the convert button and show convert another
+            convertToDogButtonOutlet.hidden = true
+            covertAnotherButtonOutlet.hidden = false
+            
+            }
+    }
+    
+    //clear everything
+    @IBAction func converAnotherButtonPressed(sender: UIButton)
+    {
+        //hide this button & show the original button
+        covertAnotherButtonOutlet.hidden = true
+        convertToDogButtonOutlet.hidden = false
+        
+        //show the text fields & clear the text
+        ageInHumanTextField.hidden = false
+        dogNameTextField.hidden = false
+        
+        ageInHumanTextField.text = ""
+        dogNameTextField.text = ""
+        
+        //hide the label
+        ageInDogLabel.hidden = true
+     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
